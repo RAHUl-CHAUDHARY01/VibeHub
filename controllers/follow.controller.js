be492ee864b73e515e68f91144949bbf5878e339
Rahul Kumar <132205464+RAHUl-CHAUDHARY01@users.noreply.github.com>
2024-09-22 18:30:45 +0530

    vibehub

--- controllers/follow.controller.js
+++ controllers/follow.controller.js
@@ -37,3 +37,10 @@ export const sendFollowRequest = async (req, res) => {
         return res.status(500).json({ message: "An error occurred while sending the follow request." });
     }
 };
+
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Akshat161/VibeHub.git
+// git push -u origin main
